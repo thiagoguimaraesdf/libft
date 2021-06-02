472579e35c5bbe5c1c9e26b0e310defe8ce29f7f
thiagoguimaraesdf <thiago_guimaraesdf10@gmail.com>
2021-06-01 21:07:53 -0300

    v1.0

--- src/ft_lstadd_front.c
+++ src/ft_lstadd_front.c
@@ -6,11 +6,10 @@
 /*   By: tguimara <tguimara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/30 08:20:47 by tguimara          #+#    #+#             */
-/*   Updated: 2021/06/01 15:52:04 by tguimara         ###   ########.fr       */
+/*   Updated: 2021/06/01 21:07:28 by tguimara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "../include/libft.h"
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
@@ -18,6 +17,6 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 	if (lst != NULL && new != NULL)
 	{
 		new->next = lst[0];
-		lst[0]->next = new;
+		lst = new;
 	}
 }
