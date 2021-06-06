570f35df5989d02365c9c8bd22d9bb987080775e
thiagoguimaraesdf <thiago_guimaraesdf10@gmail.com>
2021-06-06 15:15:05 -0300

    ft_strncmp correction

--- src/ft_strncmp.c
+++ src/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: tguimara <tguimara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/21 14:31:31 by tguimara          #+#    #+#             */
-/*   Updated: 2021/06/06 15:12:33 by tguimara         ###   ########.fr       */
+/*   Updated: 2021/06/06 15:14:54 by tguimara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	if (n == 0)
+	if (n == 0 || s1 == NULL || s2 == NULL)
 		return (0);
 	while (i < n && *(s2 + i) != '\0' && *(s1 + i) != '\0')
 	{
