3d695503482df314d18566c2c92aa6fa459b3d9e
thiagoguimaraesdf <thiago_guimaraesdf10@gmail.com>
2021-06-06 16:16:21 -0300

    ft_strjoin correction

--- src/ft_strjoin.c
+++ src/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: tguimara <tguimara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 09:16:15 by tguimara          #+#    #+#             */
-/*   Updated: 2021/06/06 16:15:22 by tguimara         ###   ########.fr       */
+/*   Updated: 2021/06/06 16:16:20 by tguimara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*ptr;
 
 	ptr = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (ptr == NULL)
+	if (ptr == NULL || s1 == NULL || s2 == NULL)
 		return (NULL);
 	i = 0;
 	while (*(s1 + i) != '\0')
