5f347526748e81b3ec721080e727af835e3b50b0
thiagoguimaraesdf <thiago_guimaraesdf10@gmail.com>
2021-06-06 15:21:41 -0300

    ft_strncmp correction

--- src/ft_strncmp.c
+++ src/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: tguimara <tguimara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/21 14:31:31 by tguimara          #+#    #+#             */
-/*   Updated: 2021/06/06 15:19:08 by tguimara         ###   ########.fr       */
+/*   Updated: 2021/06/06 15:21:13 by tguimara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,15 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
-
-	i = 0;
-	if (n == 0 || s1 == NULL || s2 == NULL)
-		return (0);
-	while (i < n && *(s2 + i) != '\0' && *(s1 + i) != '\0')
+	while (n > 0 && *s1 && *s2)
 	{
-		if (*(s1 + i) == *(s2 + i))
-			i++;
-		else
-			return ((unsigned char)(*(s1 + i)) - (unsigned char)(*(s2 + i)));
+		if ((unsigned char)(*s1) != (unsigned char)(*s2))
+			return ((unsigned char)(*s1) - (unsigned char)(*s2));
+		n--;
+		s2++;
+		s1++;
 	}
+	if (n != 0 && (unsigned char)(*s1) != (unsigned char)(*s2))
+		return ((unsigned char)(*s1) - (unsigned char)(*s2));
 	return (0);
 }
