915ed43b6b24d79fd575183ac6ec92493752a269
thiagoguimaraesdf <thiago_guimaraesdf10@gmail.com>
2021-06-06 15:35:07 -0300

    ft_atoi correction

--- src/ft_atoi.c
+++ src/ft_atoi.c
@@ -6,17 +6,15 @@
 /*   By: tguimara <tguimara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/21 14:34:42 by tguimara          #+#    #+#             */
-/*   Updated: 2021/05/21 14:34:42 by tguimara         ###   ########.fr       */
+/*   Updated: 2021/06/06 15:34:59 by tguimara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/libft.h"
-
 int	ft_atoi(const char *str)
 {
-	int	i;
-	int	res;
-	int	sign;
+	int		i;
+	long	res;
+	int		sign;
 
 	i = 0;
 	res = 0;
@@ -35,5 +33,5 @@ int	ft_atoi(const char *str)
 		res = res * 10 + (str[i] - 48);
 		i++;
 	}
-	return (res * sign);
+	return ((int)(res * sign));
 }
