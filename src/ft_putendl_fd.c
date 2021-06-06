20f6610be5173409625f7386a073f5c0f3fb4a90
thiagoguimaraesdf <thiago_guimaraesdf10@gmail.com>
2021-06-06 16:36:14 -0300

    ft_putstr  ft_putendl protection

--- src/ft_putendl_fd.c
+++ src/ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: tguimara <tguimara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/29 15:17:58 by tguimara          #+#    #+#             */
-/*   Updated: 2021/05/29 20:44:14 by tguimara         ###   ########.fr       */
+/*   Updated: 2021/06/06 16:35:37 by tguimara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ft_putendl_fd(char *s, int fd)
 {
 	int	i;
 
+	if (s == NULL)
+		return (NULL);
 	i = 0;
 	while (*(s + i) != '\0')
 	{
