6b052b770b7afcac2f7fb42a662fe86a9dd6b7bb
thiagoguimaraesdf <thiago_guimaraesdf10@gmail.com>
2021-06-06 16:49:26 -0300

    ft_lstclear correction

--- src/ft_lstclear.c
+++ src/ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: tguimara <tguimara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/30 09:20:24 by tguimara          #+#    #+#             */
-/*   Updated: 2021/06/06 16:47:41 by tguimara         ###   ########.fr       */
+/*   Updated: 2021/06/06 16:49:24 by tguimara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,9 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
 	t_list	*nextdel;
 
-	nextdel = *lst;
 	if (!lst || !del)
 		return ;
-	while (lst[0]->next != NULL)
+	while (lst[0] != NULL)
 	{
 		nextdel = lst[0]->next;
 		del(lst[0]->content);
