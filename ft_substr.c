5b3744631ede72e0dbbaf3b1d8f1656b5449dc01
thiagoguimaraesdf <thiago_guimaraesdf10@gmail.com>
2021-06-06 23:08:46 -0300

    ft_substr corrections

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: tguimara <tguimara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 08:53:45 by tguimara          #+#    #+#             */
-/*   Updated: 2021/06/06 21:12:59 by tguimara         ###   ########.fr       */
+/*   Updated: 2021/06/06 23:08:34 by tguimara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,16 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
 	char	*ptr;
+	char	*_s;
 
+	_s = (char *)s;
 	ptr = (char *)malloc(sizeof(char) * (len + 1));
-	if (ptr == NULL || s == NULL)
+	if (ptr == NULL || _s == NULL)
 		return (NULL);
 	i = 0;
-	while (i < len && start < ft_strlen(s))
+	while (i < len && start < ft_strlen(_s))
 	{
-		*(ptr + i) = *(s + start);
+		*(ptr + i) = *(_s + start);
 		i++;
 		start++;
 	}
