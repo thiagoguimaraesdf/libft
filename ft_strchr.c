9321d2570186800b3f66eb6c38edcb2852b4f0ad
thiagoguimaraesdf <thiago_guimaraesdf10@gmail.com>
2021-06-07 17:32:40 -0300

    ft_strchr  correction

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: tguimara <tguimara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 09:44:51 by tguimara          #+#    #+#             */
-/*   Updated: 2021/06/07 17:31:46 by tguimara         ###   ########.fr       */
+/*   Updated: 2021/06/07 17:32:38 by tguimara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_strchr(const char *s, int c)
 {
 	if (s == NULL)
 		return (NULL);
-	while (*s != '\0')
+	while (*s)
 	{
 		if (*s == (c % 256))
 			return ((char *)s);
