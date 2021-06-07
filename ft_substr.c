e200f3592397e21a2cfe0342cd78e79e69302652
thiagoguimaraesdf <thiago_guimaraesdf10@gmail.com>
2021-06-07 15:56:57 -0300

    ft_substr corrections

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: tguimara <tguimara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 08:53:45 by tguimara          #+#    #+#             */
-/*   Updated: 2021/06/07 15:55:58 by tguimara         ###   ########.fr       */
+/*   Updated: 2021/06/07 15:56:51 by tguimara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,28 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*dst;
-	size_t	i;
-	size_t	slen;
+	unsigned int	i;
+	unsigned int	s_len;
+	char			*substr;
 
 	if (!s)
-		return (0);
-	dst = malloc(len + 1);
-	if (!dst)
-		return (0);
-	i = 0;
-	slen = ft_strlen(s);
-	if (start < slen)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (s_len < start)
 	{
-		while (s[start + i] && i < len)
-		{
-			dst[i] = s[start + i];
-			i++;
-		}
+		if (!(substr = malloc(sizeof(char) * 1)))
+			return (NULL);
+		substr[0] = '\0';
+		return (substr);
 	}
-	dst[i] = 0;
-	return (dst);
+	if (!(substr = malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		substr[i] = s[start + i];
+		i++;
+	}
+	substr[i] = '\0';
+	return (substr);
 }
