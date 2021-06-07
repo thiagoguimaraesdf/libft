f7f9b3708c0885afc02a1029012e7ba92fef31ca
thiagoguimaraesdf <thiago_guimaraesdf10@gmail.com>
2021-06-06 23:19:22 -0300

    ft_strlcpy corrections

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: tguimara <tguimara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 09:25:01 by tguimara          #+#    #+#             */
-/*   Updated: 2021/06/06 23:03:41 by tguimara         ###   ########.fr       */
+/*   Updated: 2021/06/06 23:19:09 by tguimara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,17 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	int		i;
-	char	*_src;
-
-	_src = (char *)src;
-	i = 0;
-	if (dst == NULL || src == NULL || size == 0)
+	if (dst == NULL || src == NULL)
 		return (0);
-	while (i < (int)size - 1 && *_src != '\0')
+	if (size == 0)
+		return (ft_strlen(src));
+	while (size > 1 && *_src != '\0')
 	{
-		*(dst + i) = *_src;
-		i++;
-		_src++;
+		*dst = *src;
+		dst++;
+		*src;
+		size--;
 	}
-	if (size > 0)
-		*(dst + i) = '\0';
-	while (*_src != '\0')
-	{
-		i++;
-		_src++;
-	}
-	return ((size_t)i);
+	*dst = '\0';
+	return (ft_strlen(src));
 }
