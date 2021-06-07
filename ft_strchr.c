be7330207ab369f47b3d5735d34de8e5a8152769
thiagoguimaraesdf <thiago_guimaraesdf10@gmail.com>
2021-06-06 21:33:28 -0300

    ft_strchr corrections

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: tguimara <tguimara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 09:44:51 by tguimara          #+#    #+#             */
-/*   Updated: 2021/06/06 21:32:37 by tguimara         ###   ########.fr       */
+/*   Updated: 2021/06/06 21:33:26 by tguimara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,10 @@ char	*ft_strchr(const char *s, int c)
 	while (*(s + i) != '\0')
 	{
 		if (*(s + i) == (char)c)
-			return (s + i);
+			return ((char *)s + i);
 		i++;
 	}
 	if (*(s + i) == '\0' && (char)c == '\0')
-		return (s + i);
+		return ((char *)s + i);
 	return (NULL);
 }
