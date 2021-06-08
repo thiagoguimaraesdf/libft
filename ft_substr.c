a1193c740153b400b9f77ca4d0d4db682d08a9ce
thiagoguimaraesdf <thiago_guimaraesdf10@gmail.com>
2021-06-07 22:27:18 -0300

    ft_substr correction

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: tguimara <tguimara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 08:53:45 by tguimara          #+#    #+#             */
-/*   Updated: 2021/06/07 22:17:08 by tguimara         ###   ########.fr       */
+/*   Updated: 2021/06/07 22:26:41 by tguimara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,29 @@
 #include <stdlib.h>
 #include "libft.h"
 
-static char	*ft_substr_aux(size_t s_len, unsigned int start, size_t len)
-{
-	char	*ptr;
-	size_t	j;
-
-	if (start > s_len)
-		j = 0;
-	else if (s_len - start < len)
-		j = s_len - start;
-	else
-		j = len;
-	ptr = ft_malloc(sizeof(char) * (j + 1));
-	if (ptr != NULL)
-		*(ptr + j) = 0;
-	return (ptr);
-}
-
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t	i;
-	size_t	s_len;
-	char	*ptr;
+	unsigned int	j;
+	unsigned int	s_len;
+	char			*ptr;
 
 	if (s == NULL)
 		return (NULL);
-	ptr = ft_substr_aux(start, s_len, len);
-	if (ptr == NULL)
-		return (ptr);
 	s_len = ft_strlen(s);
-	i = 0;
-	if (start <= ft_strlen(s))
+	ptr = malloc(sizeof(char) * (len + 1));
+	if (ptr == NULL)
+		return (NULL);
+	j = 0;
+	if (start < s_len)
 	{
-		while (i < len && *(s + start + i))
+		while (len > 0 && s[start] != '\0')
 		{
-			*(ptr + i) = *(s + start + i);
-			i++;
+			ptr[j] = s[start];
+			j++;
+			start++;
+			len--;
 		}
 	}
+	ptr[j] = '\0';
 	return (ptr);
 }
